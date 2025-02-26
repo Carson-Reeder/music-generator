6c9fdfb7fbacede0478ad68272f72d01ac90e0e6
Carson-Reeder <ccreeder11@gmail.com>
2025-02-26 15:13:03 -0500

    git hub pages test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+//import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+//const nextConfig: NextConfig = {
+/* config options here */
+//};
+
+//export default nextConfig;
+
+const nextConfig = {
+  basePath: "/music-generator",
+  output: "export",
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
